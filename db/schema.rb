19461f3daff8fcd4cd26aebcf5a0ac58abbd43c3
kubaleduchowski <leduchowskijakub@gmail.com>
2017-02-09 13:52:48 +0100

    handling errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209111227) do
+ActiveRecord::Schema.define(version: 20170209112302) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
